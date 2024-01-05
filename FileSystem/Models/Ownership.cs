08481e3152175d311aafdae5669a20f19d267076
Miaad <Miaad@MIAAD>
2024-01-05 21:32:09 +0330

    Add Ownership class to FileSystem.Models

---
+++ FileSystem/Models/Ownership.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystem.Models
+{
+    public class Ownership: Entity
+    {
+        User User { get; set; }
+        File File { get; set; }
+        AccessLevels AccessLevel { get; set; }
+
+        public Ownership(User user, File file, AccessLevels accessLevel)
+        {
+            User = user;
+            File = file;
+            AccessLevel = accessLevel;
+        }
+    }
+}
