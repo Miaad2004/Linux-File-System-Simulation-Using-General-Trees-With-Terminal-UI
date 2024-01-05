88a33da9fb9b3f7daac673c90e3fb3f91e3d73a0
Miaad <Miaad@MIAAD>
2024-01-06 01:24:14 +0330

    Add LinuxDbContext class

---
+++ FileSystem/Data/LinuxDbContext.cs
@@ -0,0 +1,35 @@
+﻿using FileSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystem.Data
+{
+    public class LinuxDbContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        private readonly IConfiguration _configuration;
+
+        public LinuxDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private string GetConnectionString()
+        {
+            return _configuration.GetConnectionString("DefaultConnection") 
+                ?? throw new ConfigurationErrorsException("Invalid Configuration");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(GetConnectionString());
+        }
+    }
+}
