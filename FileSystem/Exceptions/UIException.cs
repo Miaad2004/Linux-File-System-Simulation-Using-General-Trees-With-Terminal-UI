6a0b023b0285b2e0a3fcb315a2a50115759ca1b3
Miaad <Miaad@MIAAD>
2024-01-05 21:31:24 +0330

    Add UIException class to handle UI-related exceptions

---
+++ FileSystem/Exceptions/UIException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystem.Exceptions
+{
+    public class UIException: Exception
+    {
+        public UIException()
+        {
+        }
+
+        public UIException(string? message) : base(message)
+        {
+        }
+
+        public UIException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
