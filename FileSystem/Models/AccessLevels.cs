7e0b76a07130f84351f44c1e874d3861e3231844
Miaad <Miaad@MIAAD>
2024-01-06 01:24:19 +0330

    Add AccessLevels enum to FileSystem.Models

---
+++ FileSystem/Models/AccessLevels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystem.Models
+{
+    public enum AccessLevels
+    {
+        // File Access Levels
+        Read = 1,
+        Write = 2,
+        ReadWrite = Read | Write,
+    }
+}
