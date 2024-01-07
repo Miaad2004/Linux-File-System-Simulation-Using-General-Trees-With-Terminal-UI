294015515efde1359a4033fdcb4a67467ab5ac61
Miaad <Miaad@MIAAD>
2024-01-08 01:52:18 +0330

    Add CommandOutput class to FileSystem.Models

---
+++ FileSystem/Models/CommandOutput.cs
@@ -0,0 +1,10 @@
+﻿namespace FileSystem.Models
+{
+    public class CommandOutput(bool HasFailed, Exception? exception = null)
+    {
+        public List<OutputTextDTO> OutputTexts { get; set; } = new List<OutputTextDTO>();
+        public bool HasFailed { get; set; } = HasFailed;
+
+        public Exception? exception = exception;
+    }
+}
