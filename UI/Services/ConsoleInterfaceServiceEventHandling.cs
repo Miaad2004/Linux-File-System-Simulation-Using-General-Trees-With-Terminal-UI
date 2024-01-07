7068ec769009c55c09e81be2a5ec525a118f164f
Miaad <Miaad@MIAAD>
2024-01-08 02:00:13 +0330

    Added VS solution files

---
+++ UI/Services/ConsoleInterfaceServiceEventHandling.cs
@@ -0,0 +1,69 @@
+﻿using FileSystem.Models;
+using System.Windows.Input;
+using System.Windows.Threading;
+using UI.Controls;
+using WordPredictor;
+
+namespace UI.Services
+{
+    public partial class ConsoleInterfaceService
+    {
+        private void InputControl_InlineSuggestionAccepted(object sender, InlineSuggestionEventArgs e)
+        {
+            CommandInputControl inputControl = (CommandInputControl)sender;
+            inputControl.InputTextBox.Text += e.Suggestion;
+
+            inputControl.Dispatcher.BeginInvoke(DispatcherPriority.Input,
+                    new Action(delegate ()
+                    {
+                        inputControl.InputTextBox.Focus();         // Set Logical Focus
+                        Keyboard.Focus(inputControl.InputTextBox); // Set Keyboard Focus
+                    }));
+
+            inputControl.InputTextBox.CaretIndex = inputControl.InputTextBox.Text.Length;
+
+        }
+
+        private async void InputControl_InlineSuggestionTriggred(object sender, InlineSuggestionEventArgs e)
+        {
+            CommandInputControl inputControl = (CommandInputControl)sender;
+            string lastWord = GetLastWord(e.InputText);
+            string prediction = await NextWordPredictor.Instance.Predict(lastWord, PredictionEngine.BST);
+
+            if (!prediction.StartsWith(lastWord) || prediction.Length < lastWord.Length)
+            {
+                inputControl.InLineSuggestion = "";
+                return;
+            }
+
+            string subtraction = prediction.Substring(lastWord.Length);
+            inputControl.InLineSuggestion = subtraction;
+        }
+
+        public void InputControl_EnterPressed(object sender, KeyDownEventArgs e)
+        {
+            if (((CommandInputControl)sender).IsReadonly)
+            {
+                return;
+            }
+
+            ExecuteCommand(e.InputText);
+        }
+
+        private void Node_NodeDisposed(object sender, NodeEventArgs<File> e)
+        {
+            NextWordPredictor.Instance.RemoveContextWord(e.Data.Title);
+        }
+
+        private void Node_NodeCreated(object sender, NodeEventArgs<File> e)
+        {
+            NextWordPredictor.Instance.AddContextWord(e.Data.Title);
+        }
+
+        private static string GetLastWord(string text)
+        {
+            var words = text.Split(' ');
+            return words[words.Length - 1].Trim();
+        }
+    }
+}
---
+++ UI/Services/ConsoleInterfaceServiceInputOutput.cs
@@ -0,0 +1,122 @@
+﻿using FileSystem.Models;
+using FileSystem.Services;
+using FileSystem.Services.Authentication;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using UI.Controls;
+
+namespace UI.Services
+{
+    public partial class ConsoleInterfaceService
+    {
+        private readonly List<OutputTextDTO> _textBuffer = [];
+        private static Node<File> CurrentDir => FileSystemService.Instance.CurrentDirectory;
+        private static string CurrentDirString => FileSystemService.Instance.GetCurrentDirString();
+        private static User? CurrentUser => SessionManager.CurrentSession?.User;
+
+
+        public void BufferOutputText(OutputTextDTO textObj)
+        {
+            _textBuffer.Add(textObj);
+        }
+
+        public void BufferOutputText(string text)
+        {
+            OutputTextDTO textDTO = new(text);
+            BufferOutputText(textDTO);
+        }
+
+        private void ClearBuffer()
+        {
+            var outputControl = new RichTextBox
+            {
+                Background = new SolidColorBrush(Colors.Transparent),
+                Foreground = new SolidColorBrush(Colors.White),
+                FontSize = 17,
+                IsReadOnly = true,
+                BorderThickness = new Thickness(0),
+                Margin = new Thickness(0),
+                Padding = new Thickness(0)
+            };
+
+            outputControl.Document.Blocks.Clear();
+
+            var paragraph = new Paragraph
+            {
+                Margin = new Thickness(0) // Set margin to remove any extra spacing
+            };
+
+            foreach (var textObj in _textBuffer)
+            {
+                var run = new Run(textObj.Text)
+                {
+                    Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString(textObj.HexColor)),
+                    FontWeight = textObj.IsBold ? FontWeights.Bold : FontWeights.Normal
+                };
+
+                paragraph.Inlines.Add(run);
+            }
+
+            outputControl.Document.Blocks.Add(paragraph);
+
+            stackPanel.Children.Add(outputControl);
+
+            _textBuffer.Clear();
+        }
+
+        private CommandInputControl AddInputControl(string username)
+        {
+            var inputControl = new CommandInputControl
+            {
+                Username = username,
+                CurrentDirectory = CurrentDirString
+            };
+
+            inputControl.OnEnterPressed += InputControl_EnterPressed;
+            inputControl.InlineSuggestionTriggred += InputControl_InlineSuggestionTriggred;
+            inputControl.InlineSuggestionAccepted += InputControl_InlineSuggestionAccepted;
+            stackPanel.Children.Add(inputControl);
+
+            ScrollDown();
+
+            return inputControl;
+        }
+        private CommandInputControl AddInputControl()
+        {
+            if (CurrentUser == null)
+                return AddInputControl("Authenticater");
+
+            else
+                return AddInputControl(CurrentUser.Username);
+
+        }
+
+        private void DisablePreviousInputs()
+        {
+            Keyboard.ClearFocus();
+
+            foreach (var control in stackPanel.Children)
+            {
+                if (control is CommandInputControl)
+                {
+                    ((CommandInputControl)control).IsReadonly = true;
+                    ((CommandInputControl)control).InLineSuggestion = "";
+                }
+            }
+        }
+
+        public void ClearConsole()
+        {
+            stackPanel.Children.Clear();
+            AddInputControl();
+        }
+
+        private void ScrollDown()
+        {
+            mainView.scrollViewer.ScrollToBottom();
+        }
+    }
+}
