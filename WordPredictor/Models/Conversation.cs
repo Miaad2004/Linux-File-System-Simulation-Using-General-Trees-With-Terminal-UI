9d5682eb1f83ba643beefcaa8683dc8b92f2e191
Miaad <Miaad@MIAAD>
2024-01-08 01:57:09 +0330

    Add Conversation class to WordPredictor.Models

---
+++ WordPredictor/Models/Conversation.cs
@@ -0,0 +1,27 @@
+﻿namespace WordPredictor.Models
+{
+    public class Conversation
+    {
+        public List<object> messages;
+
+        public Conversation()
+        {
+            messages = [];
+        }
+
+        public void AddSystemMessage(string content)
+        {
+            messages.Add(new { role = "system", content });
+        }
+
+        public void AddUserMessage(string content)
+        {
+            messages.Add(new { role = "user", content });
+        }
+
+        public void AddAssistantMessage(string content)
+        {
+            messages.Add(new { role = "assistant", content });
+        }
+    }
+}
