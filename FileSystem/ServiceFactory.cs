dd5ad8e256d573345e625bea4112ec2e88301c30
Miaad <Miaad@MIAAD>
2024-01-08 01:50:45 +0330

    Add ServiceFactory class for configuring services
    This commit adds a new file `ServiceFactory.cs` to the `FileSystem` directory. The `ServiceFactory` class contains a static method `ConfigureServices` that configures services for the application. The method adds scoped instances of `LinuxDbContext`, `IPasswordService`, and `IAuthenticationService`, and a singleton instance of `IConfiguration`.

---
+++ FileSystem/ServiceFactory.cs
@@ -0,0 +1,18 @@
+﻿using FileSystem.Data;
+using FileSystem.Services.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FileSystem
+{
+    public static class ServiceFactory
+    {
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<LinuxDbContext>();
+            services.AddScoped<IPasswordService, PasswordServiceSHA>();
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            services.AddSingleton(configuration);
+        }
+    }
+}
