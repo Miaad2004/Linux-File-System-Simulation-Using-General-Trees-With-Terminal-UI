dc9c69276b5c3baa524118ececc9b9696fa3f4d9
Miaad <Miaad@MIAAD>
2024-01-08 01:54:51 +0330

    Add SessionManager class for managing user sessions

---
+++ FileSystem/Services/Authentication/SessionManager.cs
@@ -0,0 +1,15 @@
+﻿using FileSystem.Models;
+
+namespace FileSystem.Services.Authentication
+{
+    public static class SessionManager
+    {
+        public static Session? CurrentSession { get; set; } = null;
+
+        public static void Revoke()
+        {
+            CurrentSession = null;
+        }
+
+    }
+}
