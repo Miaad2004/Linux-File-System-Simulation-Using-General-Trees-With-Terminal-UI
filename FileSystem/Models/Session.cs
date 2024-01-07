3c38a92a6ece1d65cdb7b4bd86da2790ec7a1cb0
Miaad <Miaad@MIAAD>
2024-01-08 01:51:55 +0330

    Refactor Session class to make it public and add User property

--- FileSystem/Models/Session.cs
+++ FileSystem/Models/Session.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FileSystem.Models
+﻿namespace FileSystem.Models
 {
-    internal class Session
+    public sealed class Session(User user) : Entity
     {
+        public User User { get; private set; } = user;
     }
 }
