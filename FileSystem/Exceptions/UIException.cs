cf2838ce7349370032d0406bc4ea439f97359e78
Miaad <Miaad@MIAAD>
2024-01-08 01:50:59 +0330

    Refactor UIException class

--- FileSystem/Exceptions/UIException.cs
+++ FileSystem/Exceptions/UIException.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FileSystem.Exceptions
+﻿namespace FileSystem.Exceptions
 {
-    public class UIException: Exception
+    public class UIException : Exception
     {
         public UIException()
         {
