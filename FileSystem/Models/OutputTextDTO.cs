0868a9aa8e81f30f14a8f3e01019d83c084d2cf6
Miaad <Miaad@MIAAD>
2024-01-08 01:53:02 +0330

    Add OutputTextDTO class to FileSystem.Models

---
+++ FileSystem/Models/OutputTextDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FileSystem.Models
+{
+    public class OutputTextDTO(string text, string hexColor = "#FFFF", bool isBold = false)
+    {
+        public string Text { get; set; } = text;
+        public string HexColor { get; set; } = hexColor;
+        public bool IsBold { get; set; } = isBold;
+    }
+}
